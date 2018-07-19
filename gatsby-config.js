ebd2cdbb3dfebdfd6c325ecadc6d3e36a9c054e3
Juan Cuadra <jcb8793@gmail.com>
2018-07-19 10:42:40 -0600

    Tracking Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,19 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-122604019-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+      },
+    }
   ],
 }
